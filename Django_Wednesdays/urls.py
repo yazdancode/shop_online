eeb1d2f6ed13de0da8e250392961351cf2660c4f
yshabanei <168936995+yshabanei@users.noreply.github.com>
2025-01-28 00:31:48 -0800

    Create Our Ecommerce Models

--- Django_Wednesdays/urls.py
+++ Django_Wednesdays/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('grappelli/', include('grappelli.urls')),
     path('', include('store.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
