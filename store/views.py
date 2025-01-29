eba689a952dfc34ab83736ab5d3c8a375b42d668
yshabanei <168936995+yshabanei@users.noreply.github.com>
2025-01-29 11:51:22 -0800

    Template Out The Website

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import SearchForm
+
 
 
 def home(request):
@@ -10,3 +12,11 @@ def home(request):
 
 def about(request):
     return render(request, "home/about.html", {})
+
+
+
+# def search(request):
+#     query = request.GET.get('q', '')
+#     results = Product.objects.filter(name__icontains=query) if query else [] 
+
+#     return render(request, 'home/navbar.html', {'results': results, 'query': query})
