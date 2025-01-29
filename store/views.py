b032582e7374b1755f7f1936e815fbab1dfe3818
yshabanei <168936995+yshabanei@users.noreply.github.com>
2025-01-29 09:11:23 -0800

    new template navaber.html

--- store/views.py
+++ store/views.py
@@ -5,3 +5,8 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, "home/index.html", {"products": products})
+
+
+
+def about(request):
+    return render(request, "home/about.html", {})
