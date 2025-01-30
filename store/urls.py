0a37c9013492ac3697c5ee9a2a3d27cd6f3297c7
yshabanei <168936995+yshabanei@users.noreply.github.com>
2025-01-30 02:27:27 -0800

    login and logout

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.home, name="home"),
-path("about/", views.about, name="about"),
-# path('search/', views.search, name='search'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    # path('search/', views.search, name='search'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import SearchForm
-
+from  django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -9,14 +9,20 @@ def home(request):
     return render(request, "home/index.html", {"products": products})
 
 
-
 def about(request):
     return render(request, "home/about.html", {})
 
 
-
 # def search(request):
 #     query = request.GET.get('q', '')
-#     results = Product.objects.filter(name__icontains=query) if query else [] 
+#     results = Product.objects.filter(name__icontains=query) if query else []
 
 #     return render(request, 'home/navbar.html', {'results': results, 'query': query})
+
+
+def login_user(request):
+    return render(request, "home/login.html", {})
+
+
+def logout_user(request):
+    pass
