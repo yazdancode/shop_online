db803cbc49dc0c0bcca531fb9311038af91a6a92
yshabanei <168936995+yshabanei@users.noreply.github.com>
2025-01-31 02:36:12 -0800

    login logout

--- store/forms.py
+++ store/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.validators import RegexValidator
 from store.models import Customer
 
+
 class SearchForm(forms.Form):
     query = forms.CharField(
         label="جستجو",
@@ -15,20 +16,24 @@ class SearchForm(forms.Form):
         ),
     )
 
+
 class CustomerForm(forms.ModelForm):
     phone = forms.CharField(
         max_length=15,
         validators=[
-            RegexValidator(regex=r"^\d{10,15}$", message="شماره تلفن باید فقط شامل اعداد باشد و بین ۱۰ تا ۱۵ رقم باشد.")
+            RegexValidator(
+                regex=r"^\d{10,15}$",
+                message="شماره تلفن باید فقط شامل اعداد باشد و بین ۱۰ تا ۱۵ رقم باشد.",
+            )
         ],
         widget=forms.TextInput(
             attrs={
                 "class": "form-control form-control-sm",
                 "placeholder": "شماره تلفن خود را وارد کنید",
                 "style": "direction:ltr; text-align:left; font-family:IRANYekan, sans-serif; font-size:10px; "
-                         "text-decoration:none solid rgb(35, 37, 78); word-spacing:0px; background-color:#FFFFFF; "
-                         "color:#23254E; height:48px; width:334.4px; border:1px solid #D32F2F; padding:8px 0px; "
-                         "display: flex; overflow:hidden; cursor:default;",
+                "text-decoration:none solid rgb(35, 37, 78); word-spacing:0px; background-color:#FFFFFF; "
+                "color:#23254E; height:48px; width:334.4px; border:1px solid #D32F2F; padding:8px 0px; "
+                "display: flex; overflow:hidden; cursor:default;",
             }
         ),
     )
